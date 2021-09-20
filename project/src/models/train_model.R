850310e141e54bc27761e507e754385df6bcf996
Giovanni Zarrillo <gazarrillo1105@gmail.com>
2021-09-20 16:04:37 -0400

    First model using logistic regression

--- project/src/models/train_model.R
+++ project/src/models/train_model.R
@@ -11,10 +11,16 @@ test <- fread('./project/volume/data/interim/test.csv')
 
 # Making a null model by taking mean of training data
 
-test$model0 <- mean(train)
+test$model0 <- mean(train$result)
+
+# Making first model using logistic regression and all variables
+
+model1 <- glm(result ~ . , data=train, family=binomial)
+
+test$model1 <- predict(model1, newdata=test, type='response')
 
 # Making submission file
 
-test$SalePrice <- test$model # replace with intended model
+test$result <- test$model0 # replace with intended model
 
-fwrite(test[, .(Id, SalePrice)], './project/volume/data/processed/submit.csv')
+fwrite(test[, .(id, SalePrice)], './project/volume/data/processed/submit.csv')
